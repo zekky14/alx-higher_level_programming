c304a5d5809a41b6eed2be515df3e5e24443a6f5
zekky14 <ezek.ogbeda@gmail.com>
2023-09-19 03:05:33 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/bash
 read url
-curl -s -o - "$url" | tail -n+2 | cut -f2- | grep 200
-if [ "$?" -eq 0 ]; then
-echo "$(echo "$url" | grep -o '<body>.*</body>')"
-else
-echo "ERROR: Response status is not 200"
-fi
+curl -s -o - "$url" | tail -n+2 | cut -f2- | grep 200 | grep -o '<body>.*</body>
