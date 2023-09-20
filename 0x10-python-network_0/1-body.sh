a52a4b10e4635ee3eb79681db647f9b9b48c86a4
zekky14 <ezek.ogbeda@gmail.com>
2023-09-20 09:21:58 -0700

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-url=$1
-curl -s $url
+# Takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s -o - -I "$1" | grep -q 200 |  echo "$(curl -s -o - -I "$1")"
